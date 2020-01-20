fd22861a6e9dc1b7aacd400b008335ad69e217cd
Pavel dub <dubstr1@gmail.com>
2020-01-21 00:23:16 +0300

    csv file loader/papaparse/xml-loader has been added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const firstConsole = 'first webpack log';
-import json from './assets/json';
+// const firstConsole = 'first webpack log';
+// import json from './assets/json';
+// import xmlFile from './assets/data.xml';
+import csvFile from './assets/data.csv';
 import './assets/styles/styles.css';
 
-console.log(firstConsole);
-console.log('JSON:',json);
+// console.log(firstConsole);
+// console.log('JSON:', json);
+// console.log('XML:', xmlFile);
+console.log('CSV:', csvFile);
