5a8eb772efc2c4d110fa28be70530a7806981827
Pavel dub <dubstr1@gmail.com>
2020-01-22 01:04:53 +0300

    webpack config has been changed

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-const sass = require('node-sass');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
