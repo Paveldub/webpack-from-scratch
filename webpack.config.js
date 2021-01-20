57ea27261d9c226451738f80884be4ab5c608cbf
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-01-21 00:14:03 +0200

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -137,6 +137,7 @@ module.exports = {
   devServer: {
     port: 3000,
     hot: isDev,
+    historyApiFallback: true,
   },
   devtool: isDev ? "source-map" : "",
   plugins: plugins(),
@@ -186,8 +187,5 @@ module.exports = {
         },
       },
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
   },
 };
