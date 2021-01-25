354b00978327bb6a8ea3396279b94e56f5ffe128
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-01-25 22:43:49 +0200

    cell component

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { FieldComponent } from "./Components/Field/Field";
 const App = () => (
   <div className="container">
     <MainTitleComponet title={"Game of life"} />
-    <FieldComponent field={"10"} onClick={() => console.log("clicked")} />
+    <FieldComponent field={[["0"]]} onClick={() => console.log("clicked")} />
   </div>
 );
 
