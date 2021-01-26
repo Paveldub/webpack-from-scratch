05902993708069e9aa23ed6a3616d57a06e3f21d
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-01-26 16:57:28 +0200

    ebanytie testi

---
+++ src/Components/Cell/Cell.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { CellComponent } from "./Cell";
+
+describe("test cell component", () => {
+  it("test click event", () => {
+    const incMock = jest.fn();
+
+    const cellClick = shallow(
+      <CellComponent onClick={incMock}>{1}</CellComponent>
+    );
+
+    expect(cellClick.text()).toEqual("1");
+
+    cellClick.find("button").simulate("click");
+
+    expect(incMock.mock.calls.length).toBe(1);
+  });
+});
