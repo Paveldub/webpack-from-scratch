b8c3bbe7ecee3d7e91f6ae93c35735df59510e03
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-02-05 00:30:27 +0200

    arr

--- src/HW3/2-request/req.tsx
+++ src/HW3/2-request/req.tsx
@@ -1,29 +1,57 @@
 import React from "react";
 
 interface Prop {
-  dataArr: number[];
+  error: string;
+  dataArr: [];
+  isLoaded: boolean;
 }
 
 export class MyComponent extends React.Component<null, Prop> {
   constructor() {
     super(null);
     this.state = {
+      error: "",
       dataArr: [],
+      isLoaded: false,
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        this.setState({
-          dataArr: data.dataArr,
-        });
-      });
+      .then(
+        (data) => {
+          console.log(data);
+          this.setState({
+            isLoaded: true,
+            dataArr: data.dataArr,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: "ERROR",
+          });
+        }
+      );
   }
 
   render() {
-    return <h1>text</h1>;
+    const { error, isLoaded, dataArr } = this.state;
+    if (error) {
+      return <div>Ошибка: {error}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <ul>
+          {dataArr.map((elem) => {
+            <li key={elem.id}>
+              {elem.name} - {elem.phone}
+            </li>;
+          })}
+        </ul>
+      );
+    }
   }
 }
