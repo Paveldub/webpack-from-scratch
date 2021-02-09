dc359549f9596ee7ccf7b88b4ebbdef76ff5a06f
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-02-09 23:53:17 +0200

    btn

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import { render } from "react-dom";
 import { MainTitleComponet } from "./Components/Field/Title";
 import { FieldComponent } from "./Components/Field/Field";
 import { ContainerStyles } from "./index.styles";
+// import { Button } from "./Components/Buttons/button";
 
 const App = () => (
   <ContainerStyles>
-    <MainTitleComponet title="Game of life" />
+    <MainTitleComponet title="Board size" />
+    <Button />
+
     <FieldComponent
       field={[["0", "", "0", "0", "", "0", "0", "", "0"]]}
       onClick={(e) => console.log(e)}
