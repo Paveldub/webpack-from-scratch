b3a71892bbed548fed4fd3a79ca9ad8d7d9c1033
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-02-10 18:39:08 +0200

    loki

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { MainTitleComponet } from "./Components/Field/Title";
-import { FieldComponent } from "./Components/Field/Field";
+import { MainTitleComponet } from "./components/Field/Title";
+import { FieldComponent } from "./components/Field/Field";
 import { ContainerStyles } from "./index.styles";
 // import { Button } from "./Components/Buttons/button";
 
@@ -10,8 +10,8 @@ const App = () => (
   <ContainerStyles>
     <MainTitleComponet title="Board size" />
 
-    <FieldComponent field={[[""]]} onClick={(e) => console.log(e)} />
+    <FieldComponent field={[[""]]} />
   </ContainerStyles>
 );
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,147 +1,36 @@
 const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const portFinderSync = require("portfinder-sync");
 
-const isDev = process.env.NODE_ENV === "development";
-const isProd = !isDev;
-
-const optimization = () => {
-  const config = {
-    splitChunks: {
-      chunks: "all",
-    },
-  };
-
-  if (isProd) {
-    config.minimizer = [new TerserWebpackPlugin()];
-  }
-
-  return config;
-};
-
-const fileName = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
-
-const babelOptions = (preset) => {
-  const opts = {
-    presets: ["@babel/preset-env"],
-    plugins: ["@babel/plugin-proposal-class-properties"],
-  };
-
-  if (preset) {
-    opts.presets.push(preset);
-  }
-
-  return opts;
-};
-
-const jsLoaders = () => {
-  const loaders = [
-    {
-      loader: "babel-loader",
-      options: babelOptions(),
-    },
-  ];
-
-  if (isDev) {
-    loaders.push("eslint-loader");
-  }
-
-  return loaders;
-};
-
-const plugins = () => {
-  const base = [
-    new HTMLWebpackPlugin({
-      template: "./index.html",
-      minify: {
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, "src/favicon.ico"),
-        to: path.resolve(__dirname, "dist"),
-      },
-    ]),
-  ];
-
-  if (isProd) {
-    base.push(
-      new BundleAnalyzerPlugin({
-        analyzerPort: "auto",
-      })
-    );
-  }
-
-  return base;
-};
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackRules = require("./webpackRules");
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
-  mode: "development",
-  entry: {
-    main: ["./index.tsx"],
-  },
-  output: {
-    filename: fileName("js"),
-    path: path.resolve(__dirname, "dist"),
-  },
+  entry: "./src/index.tsx",
+  devtool: "source-map",
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".png", ".csv"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      types: path.resolve(__dirname, "src/types"),
     },
   },
-  optimization: optimization(),
-  devServer: {
-    port: portFinderSync.getPort(3000),
-    hot: isDev,
-    historyApiFallback: true,
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
   },
-  devtool: isDev ? "source-map" : "",
-  plugins: plugins(),
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: jsLoaders(),
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.tsx$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: "babel-loader",
-          options: babelOptions("@babel/preset-typescript"),
-        },
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: "babel-loader",
-          options: babelOptions("@babel/preset-react"),
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "images",
-        },
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        loader: "file-loader",
-        options: {
-          outputPath: "fonts",
-        },
-      },
+      ...webpackRules,
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
